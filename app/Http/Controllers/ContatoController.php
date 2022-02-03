f48f483b3effc92955f9fe40ca0555e9f9eb33ba
Lucas Alixame <lucasali2003@gmail.com>
2022-02-03 16:04:02 -0300

    [ADD] METODOS PARA SALVAR DADOS DE UM FORMULARIO NO BANCO DE DADOS

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller{
 
-    public function contato(){
+    /**
+     *  METODO RESPONSAVEL POR RENDERIZAR A PAGINA 'ENTRE EM CONTATO'
+     *
+     * @param Request $formData - VARIAVEL QUE RECEBE OS DADOS DO FORMULARIO
+     * @return view
+     */
+    public function contato(Request $formData){
 
-        return view('site.contato');
+        // 1º METODO DE SALVAR O DADO DO FORMULARIO NO BANCO (METODO UTILIZADO PARA TRATAMENTO DE INFORMAÇÃO)
+        /*
+        $contato = new SiteContato();
+        $contato->nome = $formData->input("nome");
+        $contato->telefone = $formData->input("telefone");
+        $contato->email = $formData->input("email");
+        $contato->motivo_contato = $formData->input("motivo_contato");
+        $contato->mensagem = $formData->input("mensagem");
+        $contato->save();
+        */
+
+        // 2º METODO DE SALVAR O DADO DO FORMULARIO NO BANCO (METODO PRATICO)
+        $contato = new SiteContato();
+        $contato->fill($formData->all());
+        $contato->save();
+
+        // 3º METODO DE SALVAR O DADO DO FORMULARIO NO BANCO (METODO RAPIDO)
+        /*
+        $contato = new SiteContato();
+        $contato->create($formData->all());
+        */
+
+        return view('site.contato', ['titulo' => 'Super Gestão | Site Contato']);
 
     }
 }
--- app/SiteContato.php
+++ app/SiteContato.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {   
+     // Definindo nome correto da tabela no banco de dados
+     protected $table = "site_contatos";
+
+     // Definindo campos da tabela que podem ser recuperados e alterados
+     protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
+
+
         /**
          * Undocumented function
          *
