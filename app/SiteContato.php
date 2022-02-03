442cf6bc21a302d65f3c0a9db8b1572516980972
Lucas Alixame <lucasali2003@gmail.com>
2022-02-03 14:53:47 -0300

    teste de select all

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -5,6 +5,30 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
-{
-    
+{   
+        /**
+         * Undocumented function
+         *
+         * @param Array $contatos
+         * @return void
+         */
+        static function mostrar(Array $contatos){
+
+            $i = 0;
+
+            while($i <= 100){
+
+                echo '
+                ID: '.$contatos[$i]['id'].'
+                Nome: '.$contatos[$i]['nome'].'
+                Telefone: '.$contatos[$i]['telefone'].'
+                Email: '.$contatos[$i]['email'].'
+                Motivo: '.$contatos[$i]['motivo_contato'].'
+                Mensagem: '.$contatos[$i]['mensagem'].'
+                -------------------------------------------';
+
+                $i++;
+
+            }
+        }
 }
