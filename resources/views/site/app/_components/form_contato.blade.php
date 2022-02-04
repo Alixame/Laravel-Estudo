ae21d3da37a5e2015adc2693404ef9272320b9f9
Lucas Alixame <lucasali2003@gmail.com>
2022-02-04 09:51:02 -0300

    [ADD] RECUPERAÇÃO DE DADOS DO FORMULARIO APÓS ALGUM ERRO

--- resources/views/site/app/_components/form_contato.blade.php
+++ resources/views/site/app/_components/form_contato.blade.php
@@ -4,11 +4,11 @@
 
 <form action="{{ route('site.contato') }}" method="POST">
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="{{ $class }}">
+    <input value="{{ old('nome') }}" name="nome" type="text" placeholder="Nome" class="{{ $class }}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="{{ $class }}">
+    <input value="{{ old('telefone') }}" name="telefone" type="text" placeholder="Telefone" class="{{ $class }}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="{{ $class }}">
+    <input value="{{ old('email') }}" name="email" type="text" placeholder="E-mail" class="{{ $class }}">
     <br>
     <select name="motivo_contato" class="{{ $class }}">
         <option value="">Qual o motivo do contato?</option>
@@ -17,7 +17,7 @@
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class="{{ $class }}" placeholder="Preencha aqui a sua mensagem"></textarea>
+    <textarea name="mensagem" class="{{ $class }}" placeholder="Preencha aqui a sua mensagem">@if(old('mensagem') != '') {{ old('mensagem') }} @endif</textarea>
     <br>
     <button type="submit" class="{{ $class }}">ENVIAR</button>
 </form>
